245a81e187d75f1f360b474ca83199aff62bd77c
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-01-14 13:29:35 -0300

    🎉 initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,19 @@
+new Tooltip(document.getElementById("github"), {
+  title: "sasalatart",
+  placement: "bottom"
+});
+
+new Tooltip(document.getElementById("linkedin"), {
+  title: "SASalataRT",
+  placement: "bottom"
+});
+
+new Tooltip(document.getElementById("twitter"), {
+  title: "@SASalataRT",
+  placement: "bottom"
+});
+
+new Tooltip(document.getElementById("cv"), {
+  title: "See CV",
+  placement: "bottom"
+});
