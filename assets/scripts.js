e6f31d7b4d203edb2b14cdc583fb918ce13cb8c0
Sebastián Salata R-T <sebastian@salatart.com>
2019-05-26 19:18:56 -0400

    ✨ improve responsiveness

--- assets/scripts.js
+++ assets/scripts.js
@@ -22,6 +22,5 @@ new Granim({
 
 new Tooltip(document.getElementById('github'), { title: 'sasalatart', placement: 'bottom' });
 new Tooltip(document.getElementById('linkedin'), { title: 'SASalataRT', placement: 'bottom' });
-new Tooltip(document.getElementById('telegram'), { title: '@SASalataRT', placement: 'bottom' });
 new Tooltip(document.getElementById('twitter'), { title: '@SASalataRT', placement: 'bottom' });
 new Tooltip(document.getElementById('resume'), { title: 'Resume', placement: 'bottom' });
