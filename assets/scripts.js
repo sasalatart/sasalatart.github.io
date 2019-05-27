f9565519929697d0a741851d49b5a909d23b3858
Sebastián Salata R-T <sebastian@salatart.com>
2019-05-26 21:19:24 -0400

    ✨ change resume link into a skills modal

--- assets/scripts.js
+++ assets/scripts.js
@@ -23,4 +23,6 @@ new Granim({
 new Tooltip(document.getElementById('github'), { title: 'sasalatart', placement: 'bottom' });
 new Tooltip(document.getElementById('linkedin'), { title: 'SASalataRT', placement: 'bottom' });
 new Tooltip(document.getElementById('twitter'), { title: '@SASalataRT', placement: 'bottom' });
-new Tooltip(document.getElementById('resume'), { title: 'Resume', placement: 'bottom' });
+new Tooltip(document.getElementById('skills'), { title: 'Skills', placement: 'bottom' });
+
+MicroModal.init();
