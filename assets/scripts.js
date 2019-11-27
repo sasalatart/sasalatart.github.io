287b47bcbfa1af303f4d9a06035af29e3e3ea2ae
Sebastián Salata R-T <sa.salatart@gmail.com>
2019-11-26 22:36:10 -0300

    🎨 update layout

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,28 +1,18 @@
-new Granim({
-  element: '#background',
-  direction: 'top-bottom',
-  opacity: [1, 1],
-  isPausedWhenNotInView: true,
-  image : {
-    source: 'assets/images/background.jpg',
-    blendingMode: 'multiply',
-    stretchMode: ['stretch-if-smaller', 'stretch-if-smaller'],
-  },
-  states : {
-    "default-state": {
-      gradients: [
-        ['#ff6b6b', '#556270'],
-        ['#80d3fe', '#7ea0c4'],
-        ['#f0ab51', '#eceba3'],
-      ],
-      transitionSpeed: 7000,
-    },
-  },
+new Tooltip(document.getElementById("github"), {
+  title: "sasalatart",
+  placement: "bottom"
 });
-
-new Tooltip(document.getElementById('github'), { title: 'sasalatart', placement: 'bottom' });
-new Tooltip(document.getElementById('linkedin'), { title: 'SASalataRT', placement: 'bottom' });
-new Tooltip(document.getElementById('twitter'), { title: '@SASalataRT', placement: 'bottom' });
-new Tooltip(document.getElementById('skills'), { title: 'Skills', placement: 'bottom' });
+new Tooltip(document.getElementById("linkedin"), {
+  title: "SASalataRT",
+  placement: "bottom"
+});
+new Tooltip(document.getElementById("twitter"), {
+  title: "@SASalataRT",
+  placement: "bottom"
+});
+new Tooltip(document.getElementById("skills"), {
+  title: "Skills",
+  placement: "bottom"
+});
 
 MicroModal.init();
